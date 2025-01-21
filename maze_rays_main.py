c1f0d9f660f273ac46f4ed96de40f46bf2a09f86
unknown <jaredtiu1706@gmail.com>
2025-01-21 21:26:08 +0800

    added functions for user input in arrow keys

--- maze_rays_main.py
+++ maze_rays_main.py
@@ -18,9 +18,27 @@ def main():
 
     p1 = Particle((20,20), 500)
 
-    left, right, forward, Reverse = False, False, False, False 
+    left, right, forward, reverse = False, False, False, False 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit
                 exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    left = True 
+                if event.key == pygame.K_RIGHT:
+                    right = True 
+                if event.key == pygame.K_UP:
+                    forward = True
+                if event.key == pygame.K_DOWN:
+                    Reverse = True
+            if event.type == pygame.K_UP:
+                if event.key == pygame.K_LEFT: 
+                    left = False
+                if event.key == pygame.K_RIGHT: 
+                    right = False 
+                if event.key == pygame.K_UP:
+                    forward = False
+                if event.key == pygame.K_DOWN: 
+                    reverse = False
