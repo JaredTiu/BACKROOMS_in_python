fc8bb5f7375b7ed6fa296359391c02d34063d203
unknown <jaredtiu1706@gmail.com>
2025-01-21 16:09:49 +0800

    added def to check the neighboring cells

--- maze_creation.py
+++ maze_creation.py
@@ -7,7 +7,7 @@ class Cell:
         self.side = side
         self.visited = False
         self.walls = []
-        self.neighbors = []
+        self.find_other_cells = []
 
         self.creating_walls()
 
@@ -15,7 +15,15 @@ class Cell:
         top_right = (self.pos[0]+self.side, self.pos[1])
         bottom_right = (self.pos[0]+self.side, self.pos[1]+ self.side)
         bottom_left = (self.pos[0], self.pos[1]+ self.side)
+        #adding the cells to the list
         self.walls.append([self.pos, top_right])
         self.walls.append([bottom_left, bottom_right])
         self.walls.append([self.pos, bottom_left])
         self.walls.append([top_right, bottom_right])
+
+    def find_other_cells(self, grid):
+        for cell in grid:
+            if math.dist(self.pos, cell.pos) == self.side:
+                self.find_other_cells.append(cell)
+
+    
