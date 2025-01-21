b74414fe4bda7ff8158f4600468fe37b762fd100
unknown <jaredtiu1706@gmail.com>
2025-01-21 21:10:09 +0800

    added maze reference and final changes to rays, THIS IS JUST FOR TESTING OF RAYS

--- making_of_the_rays.py
+++ making_of_the_rays.py
@@ -84,3 +84,11 @@ class Ray:
                     if distance_check < distance: 
                         distance = distance_check
                         minimum_terminus = (x_point, y_point)
+
+            if distance != self.length:
+                self.terminus = minimum_terminus
+                return
+        
+        point_x = self.pos[0] + self.dir[0] * self.length
+        point_y = self.pos[1] + self.dir[1] * self.length
+        self.terminus = (point_x, point_y)
