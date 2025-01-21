2f337c09d7c3c22c145e1fade43319773003db8a
unknown <jaredtiu1706@gmail.com>
2025-01-21 20:06:20 +0800

    added def for grouped walls

--- making_of_the_rays.py
+++ making_of_the_rays.py
@@ -15,3 +15,29 @@ class Particle:
         grouped_walls = self.grouped_walls(walls)
         for ray in self.rays:
             ray.update(self.pos, self.dir, grouped_walls)
+
+    def grouped_walls(self, walls):
+        distances = []
+        for wall in walls:
+            distance_a = math.dist(wall[0], self.pos)
+            distance_b = math.dist(wall[1], self.pos)
+            if distance_a <= distance_b:
+                distances.append(distance_a)
+            else:
+                distances.append(distance_b)
+        ordered_walls = [x for _, x in sorted(zip(distances, walls))]
+
+        distances = sorted(distances)
+        grouped_walls = []
+        temp = [ordered_walls[0]]
+
+        for i in range(1, len(distances)-1):
+            if distances[i] == distances[i-1]:
+                temp.append(ordered_walls[i])
+
+            else:
+                grouped_walls.append(temp)
+                temp = [ordered_walls[i]]
+        
+        grouped_walls.append(temp)
+        return grouped_walls
