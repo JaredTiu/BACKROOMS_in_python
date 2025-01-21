03efd8f6e4099ee425df886d0de2579c73109ad8
unknown <jaredtiu1706@gmail.com>
2025-01-21 15:38:14 +0800

    setting the parameters for a simple cell for the maze

--- maze_creation.py
+++ maze_creation.py
@@ -1,2 +1,13 @@
-#this is for creating the maze for the main backroom game
+import math
 
+#this is the math for creating the maze for the main backroom game
+class Cell: 
+        def __init__(self, position, side):
+            self.pos = position
+            self.side = side
+            self.visited = False
+            self.walls = []
+
+            self.create_walls()
+
+            
