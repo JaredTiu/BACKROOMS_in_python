22f04c2df543bb9e7dcb0dbad9ed56c0ea0b0fd3
unknown <jaredtiu1706@gmail.com>
2025-01-23 19:29:32 +0800

    added functions as same as the other tests but changed the range in the initialize function

--- Backrooms_support.py
+++ Backrooms_support.py
@@ -1,2 +1,20 @@
 #this will be a file support for backrooms main 
+import math
 
+class Particle: 
+    def __init__(self, position, ray_length):
+        self.pos = position
+        self.ray_length = ray_length
+        self.dir = 0
+        self.rays = []
+        for angle in range(-300, 300, 5):
+            ray = Ray(self.pos, angle, self.ray_length)
+            self.rays.append(ray)
+
+    def update(self, point, walls):
+        self.pos = point
+        grouped_walls = self.grouped_walls(walls)
+        for ray in self.rays:
+            ray.update(self.pos, self.dir, grouped_walls)
+
+    
