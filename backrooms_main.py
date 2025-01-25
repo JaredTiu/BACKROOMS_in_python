eb4eb3c2131e256d233b5afd93c03560bf44c20b
unknown <jaredtiu1706@gmail.com>
2025-01-26 04:13:59 +0800

    added function for screen and visual elements

--- backrooms_main.py
+++ backrooms_main.py
@@ -6,3 +6,18 @@ from sys import exit
 from maze_creation import generate_maze
 from Backrooms_support import Particle
 
+def main():
+    pygame.init()
+    width, height = 1200, 400
+    screen = pygame.display.set.mode((width, height))
+    clock = pygame.time.Clock()
+
+    background = pygame.surface((width, height))
+    background.fill((20,20,20))
+    ceiling = pygame.Surface((width//2, height//2))
+    ceiling.fill((87,82,73))
+    floor = pygame.Surface((width//2, height//2))
+    floor.fill((113,82,41))
+    
+    wall_texture = pygame.image.load('stonewall.png').convert()
+    wall_texture = pygame.transform.scale(wall_texture, (400, 100))
