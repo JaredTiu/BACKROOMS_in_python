937570600d449464ce49336765e47ea1cc959d1b
unknown <jaredtiu1706@gmail.com>
2025-01-26 04:02:50 +0800

    added libraries and files needed

---
+++ backrooms_main.py
@@ -0,0 +1,8 @@
+#this will be the main code for the main game. 
+
+import pygame
+import math
+from sys import exit
+from maze_creation import generate_maze
+from Backrooms_support import Particle
+
