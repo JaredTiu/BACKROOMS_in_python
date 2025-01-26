21d11f8eddcdaf738cd1e682d14f26ce7dbf3772
unknown <jaredtiu1706@gmail.com>
2025-01-26 18:24:27 +0800

    fixed frame rate drops, but the walls are not visible and are enlarged now

--- backrooms_main.py
+++ backrooms_main.py
@@ -8,11 +8,11 @@ from Backrooms_support import Particle
 
 def main():
     pygame.init()
-    width, height = 1200, 400
+    width, height = 800, 600
     screen = pygame.display.set_mode((width, height))
     clock = pygame.time.Clock()
 
-    background_color = (20,20,20)
+    background_color = (20, 20, 20)
     # background = pygame.Surface((width, height))
     # background.fill((20,20,20))
     # ceiling = pygame.Surface((width//2, height//2))
@@ -21,12 +21,13 @@ def main():
     # floor.fill((113,82,41))
     
     wall_texture = pygame.image.load('stonewall.png').convert()
-    wall_texture = pygame.transform.scale(wall_texture, (400, 100))
+    wall_texture_scaled = [pygame.transform.scale(wall_texture, (400, h)) for h in range(1, 401, 10)]
 	
-    p1 = Particle((20,20), 500)
+    p1 = Particle((20,20), 250)
     maze = generate_maze(width, height, 40)
 	
     left, right, forward, reverse = False, False, False, False 
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -58,7 +59,7 @@ def main():
         if right: 
             p1.dir += 20 
         if forward:
-            angle = math.radians((p1.dir)/10)
+            angle = math.radians(p1.dir / 10)
             x = p1.pos[0] + (1 * math.cos(angle))
             y = p1.pos[1] + (1 * math.sin(angle))
             new_pos = (x, y)
@@ -76,7 +77,6 @@ def main():
         screen.fill(background_color)
         
         slice_w = width / len(p1.rays)
-        offset = 0
         for i, ray in enumerate(p1.rays):
             if ray.active_wall:
                 if ray.terminus[0] == ray.active_wall[0][0]:
@@ -91,15 +91,18 @@ def main():
 
             if h > height:
                 h = height
-            w = h * 4
-            y = (height/2) - (h/2)
-            temp_image = pygame.transform.scale(wall_texture, (w, h))
-            screen.blit(temp_image, (offset+(i*slice_w), y), (img_start,0,slice_w,h))
+
+                # Use precomputed textures for performance
+                index = min(len(wall_texture_scaled) - 1, int(h / 10))
+                temp_image = wall_texture_scaled[index]
+                y = (height / 2) - (h / 2)
+                screen.blit(temp_image, (i * slice_w, y), (img_start, 0, slice_w, h))
 
         pygame.display.update()
         clock.tick(30)
+        # print(f"particle position: {p1.pos}")
+        # print(f"Number of rays: {len(p1.rays)}")
 
 if __name__ == '__main__':
     main()
-
     
